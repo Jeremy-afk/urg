508ce96b3f91b1b4a6f6c8ca30e7977f45eaecfe
AdriKat2022 <adrien.schroedel91@gmail.com>
2024-11-08 17:58:56 +0100

    moved some files and made the jeremy scene playable online on the NetworkCheckpoint Scene

--- Assets/Scripts/Movements.cs
+++ Assets/Scripts/Movements.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class NewBehaviourScript : MonoBehaviour
+public class Movements : MonoBehaviour
 {
     private Rigidbody rigidBody;
 
