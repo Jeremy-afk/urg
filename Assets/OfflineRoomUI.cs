9bf18217dd9aff30befede967fa96ed2c878e9b4
Mortox <steeven_yugo@hotmail.fr>
2025-01-20 14:57:10 +0100

    Add OfflineRoomUI

---
+++ Assets/OfflineRoomUI.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.EditorTools;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OfflineRoomUI : MonoBehaviour
+{
+    [SerializeField] private Button createRoomButton;
+    [SerializeField] private Button joinRoomButton;
+
+
+
+    void Start()
+    {
+        createRoomButton.onClick.AddListener(StartServer);
+        joinRoomButton.onClick.AddListener(StartClient);
+    }
+    private void StartServer()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void StartClient()
+    {
+        // Récupérer l'adresse du serveur
+        string address = MyNetworkRoomManager.singleton.networkAddress;
+
+        // Démarrer le client
+        MyNetworkRoomManager.singleton.StartClient();
+    }
+
+}
