7d7c4106819b0f4260c61ea0588edf020b2783e0
AdriKat2022 <adrien.schroedel91@gmail.com>
2025-01-30 03:42:52 +0100

    made animations and popup now takes back to offline room

--- Assets/Scripts/UI/ContestantDisplay.cs
+++ Assets/Scripts/UI/ContestantDisplay.cs
@@ -3,10 +3,15 @@ using UnityEngine;
 
 public class ContestantDisplay : MonoBehaviour
 {
-    [SerializeField] private RectTransform leaderboardContainer; // Parent for contestants
+    [SerializeField] private GameObject finishedText;
+    [SerializeField] private RectTransform leaderboardWindow; // Parent for contestants
+    [SerializeField] private RectTransform contestantsContainer; // Parent for contestants
     [SerializeField] private Contestant contestantPrefab;
 
     [Header("Animation")]
+    [SerializeField] private float finishedRaceDelay = 2f;
+    [SerializeField] private float leaderboardShowDelay = 3f;
+    [SerializeField] private float leaderboardAnimationDelay = 1f;
     [SerializeField] private float contestantAnimationDelay = 0.1f;
 
     [Header("Debug")]
@@ -23,13 +28,15 @@ public class ContestantDisplay : MonoBehaviour
 
     public void DisplayContestants(ContestantData[] contestants)
     {
+        finishedText.SetActive(false);
+        leaderboardWindow.gameObject.SetActive(false);
         gameObject.SetActive(true);
         StartCoroutine(SpawnContestants(contestants));
     }
 
     private void ClearContestants()
     {
-        foreach (Transform child in leaderboardContainer)
+        foreach (Transform child in contestantsContainer)
         {
             Destroy(child.gameObject);
         }
@@ -37,9 +44,19 @@ public class ContestantDisplay : MonoBehaviour
 
     private IEnumerator SpawnContestants(ContestantData[] contestants)
     {
+        yield return new WaitForSeconds(finishedRaceDelay); // Delay before showing finished text
+
+        finishedText.SetActive(true);
+
+        yield return new WaitForSeconds(leaderboardShowDelay); // Delay before showing leaderboard
+
+        leaderboardWindow.gameObject.SetActive(true);
+
+        yield return new WaitForSeconds(leaderboardAnimationDelay); // Delay before spawning contestants
+
         foreach (var contestant in contestants)
         {
-            Contestant newContestant = Instantiate(contestantPrefab, leaderboardContainer);
+            Contestant newContestant = Instantiate(contestantPrefab, contestantsContainer);
             newContestant.SetContestantName(contestant.Name);
             newContestant.SetPlacement(contestant.Placement);
             newContestant.SetIsYou(contestant.IsYou);
