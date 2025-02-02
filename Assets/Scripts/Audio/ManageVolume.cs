0671576d1910fb7e5797554d937cc0d22c80859b
greenreeper1 <cedricba.gautier@gmail.com>
2025-02-02 16:53:03 +0100

    Make settings menu

---
+++ Assets/Scripts/Audio/ManageVolume.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManageVolume : MonoBehaviour
+{
+    public void SetVolumeMusic(float volumeMusic)
+    {
+        Debug.Log(volumeMusic);
+    }
+
+    public void SetVolumeSounds(float volumeSounds)
+    {
+        Debug.Log(volumeSounds);
+    }
+}
---
+++ Assets/Scripts/UsernameManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UsernameManager : MonoBehaviour
+{ 
+    public void SetUsername(string username)
+    {
+        Debug.Log(username);
+    }
+}
