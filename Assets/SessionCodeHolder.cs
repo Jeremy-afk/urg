580ea7ede49375a4561f9d5e01e8ac094a8bb11f
Mortox <steeven_yugo@hotmail.fr>
2025-02-03 08:49:30 +0100

    Bug fix: session code shared through all clients

--- Assets/SessionCodeHolder.cs
+++ Assets/SessionCodeHolder.cs
@@ -1,3 +1,4 @@
+using Mirror;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -5,8 +6,9 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class SessionCodeHolder : MonoBehaviour
+public class SessionCodeHolder : NetworkBehaviour
 {
+    [SyncVar]
     public string sessionCode = "";
 
     private void Awake()
@@ -26,6 +28,20 @@ public class SessionCodeHolder : MonoBehaviour
         if (sceneLoaded.name == "RoomOnline")
         {
             GameObject.Find("SessionCodeText").GetComponent<TextMeshProUGUI>().text = "Session code : " + sessionCode;
+        }
+    }
+
+    public override void OnStartClient()
+    {
+        base.OnStartClient();
+
+        try
+        {
+            GameObject.Find("SessionCodeText").GetComponent<TextMeshProUGUI>().text = "Session code : " + sessionCode;
+        }
+        catch (Exception e)
+        {
+            Debug.Log("No session code text UI found in this scene.");
         }
     }
 }
