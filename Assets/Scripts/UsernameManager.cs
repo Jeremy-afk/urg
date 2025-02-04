fc0c84a20ac6bcb4122b2393b8a10bfd8cefa6a4
AdriKat2022 <adrien.schroedel91@gmail.com>
2025-02-04 02:58:12 +0100

    username tweak

--- Assets/Scripts/UsernameManager.cs
+++ Assets/Scripts/UsernameManager.cs
@@ -1,11 +1,12 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class UsernameManager : MonoBehaviour
 {
     [SerializeField] private TMP_InputField usernameInputField;
     [SerializeField] private string playprefKey = "Username";
-
+    [SerializeField] private UnityEvent<string> onNewUsername;
     public string Username { get; private set; }
 
     private void OnEnable()
@@ -20,16 +21,19 @@ public class UsernameManager : MonoBehaviour
     private void OnDisable()
     {
         SaveUsername();
+        onNewUsername.Invoke(Username);
     }
 
     public void SaveUsername()
     {
-        Username = usernameInputField.text;
-        PlayerPrefs.SetString(playprefKey, Username);
-    }
-
-    public void SetUsername(string username)
-    {
-        this.Username = username;
+        if (string.IsNullOrEmpty(Username))
+        {
+            PlayerPrefs.DeleteKey(playprefKey);
+        }
+        else
+        {
+            Username = usernameInputField.text;
+            PlayerPrefs.SetString(playprefKey, Username);
+        }
     }
 }
